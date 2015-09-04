ba71f3bfd521b043b23d27393fa78050efb8af1e
Marcin Górczyński <marcin.gorczynski@netguru.pl>
2015-09-04 17:22:50 +0200

    mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   # config.active_record.dump_schema_after_migration = false
+  config.action_mailer.smtp_settings = {
+    :address   => ENV["MAIL_HOST"],
+    :port      => ENV["MAIL_PORT"], # ports 587 and 2525 are also supported with STARTTLS
+    :enable_starttls_auto => true, # detects and uses STARTTLS
+    :user_name => ENV["MAIL_USER"],
+    :password  => ENV["MAIL_PASS"], # SMTP password is any valid API key
+    :authentication => 'login', # Mandrill supports 'plain' or 'login'
+    :domain => ENV["MAIL_DOMAIN"], # your domain to identify your server when connecting
+  }
+
 end
